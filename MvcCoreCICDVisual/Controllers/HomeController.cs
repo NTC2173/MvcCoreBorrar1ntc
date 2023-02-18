fc22a39b9530a3d2727c6dcdf072fda62ed4dd1d
Nacho_21 <igntc93@gmail.com>
2023-02-18 23:36:12 +0100

    Add project files.

---
+++ MvcCoreCICDVisual/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCICDVisual.Models;
+using System.Diagnostics;
+
+namespace MvcCoreCICDVisual.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //CODIGO DE SERVIDOR Y CODIGO DE CLIENTE
+            ViewData["SALUDO"] = "Bienvenido a mi App";
+            Random random = new Random();
+            List<int> numeros = new List<int>();
+            for(int i=0; i < 10; i++)
+            {
+                int aleat = random.Next(1, 100);
+                numeros.Add(aleat);
+            }
+
+            return View(numeros);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
